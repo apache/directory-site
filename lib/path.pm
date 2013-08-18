e5c0bd64c4ebee5b442f7a7e04f3ca1d7c981e71
elecharny <elecharny@apache.org>
2013-08-18 23:37:05 +0000

    Updated to the latest revision for the LDAP API and ApacheDS
    git-svn-id: https://svn.apache.org/repos/asf/directory/site/trunk@1515243 13f79535-47bb-0310-9956-ffa450edef68

--- lib/path.pm
+++ lib/path.pm
@@ -1,8 +1,8 @@
 package path;
 
 # Versions (for download links)
-our $version_api = "1.0.0-M19";
-our $version_apacheds = "2.0.0-M14";
+our $version_api = "1.0.0-M20";
+our $version_apacheds = "2.0.0-M15";
 our $version_studio = "2.0.0.v20130628";
 our $version_studio_name = "2.0.0-M8";
 our $version_mavibot = "1.0.0-M1";
