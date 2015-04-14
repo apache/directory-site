a5a98da255bd88eba24afe45fd634785c17f79b2
elecharny <elecharny@apache.org>
2015-04-14 07:02:05 +0000

    Fixed some links for Fortress-core download
    git-svn-id: https://svn.apache.org/repos/asf/directory/site/trunk@1673374 13f79535-47bb-0310-9956-ffa450edef68

--- lib/path.pm
+++ lib/path.pm
@@ -7,7 +7,7 @@ our $version_studio = "2.0.0.v20130628";
 our $version_studio_name = "2.0.0-M8";
 our $version_mavibot = "1.0.0-M7";
 our $version_escimo = "1.0.0-M1";
-our $version_fortress = "1.0.0-RC39";
+our $version_fortress = "1.0-RC40";
 our $version_kerby = "1.0.0-M1";
 
 
