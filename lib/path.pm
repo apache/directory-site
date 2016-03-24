5599c3c80cb1c5be298a9ca8f08c90844f52510b
smckinney <smckinney@apache.org>
2016-03-24 17:33:18 +0000

    1.0-RC42
    git-svn-id: https://svn.apache.org/repos/asf/directory/site/trunk@1736471 13f79535-47bb-0310-9956-ffa450edef68

--- lib/path.pm
+++ lib/path.pm
@@ -7,7 +7,7 @@ our $version_studio = "2.0.0.v20151221-M10";
 our $version_studio_name = "2.0.0-M10";
 our $version_mavibot = "1.0.0-M8";
 our $version_escimo = "1.0.0-M1";
-our $version_fortress = "1.0-RC40";
+our $version_fortress = "1.0-RC42";
 our $version_kerby = "1.0.0-RC2";
 
 
