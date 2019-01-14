e133f15dbf287c774282070f1a5428cbaff99bd6
coheigea <coheigea@apache.org>
2019-01-14 14:20:28 +0000

    Updating website for Kerby 2.0.0 release
    git-svn-id: https://svn.apache.org/repos/asf/directory/site/trunk@1851256 13f79535-47bb-0310-9956-ffa450edef68

--- lib/path.pm
+++ lib/path.pm
@@ -9,7 +9,7 @@ our $version_studio_name = "2.0.0-M14";
 our $version_mavibot = "1.0.0-M8";
 our $version_scimple = "1.0.0";
 our $version_fortress = "2.0.3";
-our $version_kerby = "1.1.1";
+our $version_kerby = "2.0.0";
 
 
 # All our pages use the same view function
